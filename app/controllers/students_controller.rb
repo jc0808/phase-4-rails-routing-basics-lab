b6990858647f70b7b65bd54b5be72421040a4ac1
Joshua Campos <jdeveloper1203@gmail.com>
2022-11-04 14:52:14 -0400

    commit tree e44d5f1d4a898345789586f6489ee3da003072da

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,17 @@
+class StudentsController < ApplicationController
+
+    def index
+        students = Student.all
+        render json: students
+    end
+
+    def grades
+        grades = Student.order(grade: :desc)
+        render json: grades
+    end
+
+    def highest_grade
+        grades = Student.order(grade: :desc)
+        render json: grades.first
+    end
+end
